2e0553aee6b51b72cc3f3525f72217af14d297b9
Mari Sumida Miyashiro <mariksm@hawaii.edu>
2023-08-29 14:21:13 -1000

    changed "x" and added animation to main page

--- js/label.js
+++ js/label.js
@@ -13,46 +13,47 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   function addIngredient(){
-   let ingredient = document.getElementById("fruitSelect");
-   let newIngredient = document.getElementById("newIngredient");
-   let amount = document.getElementById("amount")
-    let result="";
-   if(ingredient.value !="default"){
-    console.log(ingredient.value)
-    result = ingredient.value;
-   }
-   else{
-    console.log(newIngredient.value)
-    result = newIngredient.value;
-   }
-   let ingredientList = document.getElementById("ingredients");
-   let newItem = document.createElement("li");
-   let name = document.createElement("span");
-   let ounces = document.createElement("span");
-   let button = document.createElement("button");
-   button.textContent = "X";
-   button.onclick = function(){
-    this.parentElement.remove();
-   }
-   name.textContent=result;
-   name.className=("ingredientSpan")
-   ounces.textContent= amount.value + " oz"
-   ounces.className=("amountSpan")
-   newItem.append(name)
-   newItem.append(ounces)
-   newItem.append(button)
-   ingredientList.append(newItem);
+    let ingredient = document.getElementById("fruitSelect");
+    let newIngredient = document.getElementById("newIngredient");
+    let amount = document.getElementById("amount")
+      let result="";
+    if(ingredient.value !="default"){
+      console.log(ingredient.value)
+      result = ingredient.value;
+    }
+    else{
+      console.log(newIngredient.value)
+      result = newIngredient.value;
+    }
+    let ingredientList = document.getElementById("ingredients");
+    let newItem = document.createElement("li");
+    let name = document.createElement("span");
+    let ounces = document.createElement("span");
+    let button = document.createElement("button");
+    button.textContent = "X";
+    button.className=("buttonx"); 
+    button.onclick = function(){
+      this.parentElement.remove();
+    }
+    name.textContent=result;
+    name.className=("ingredientspan")
+    ounces.textContent= amount.value + " oz"
+    ounces.className=("amountspan")
+    newItem.append(name)
+    newItem.append(ounces)
+    newItem.append(button)
+    ingredientList.append(newItem);
 
-   ingredient.value="default";
-   newIngredient.value="";
-   amount.value="";
+    ingredient.value="default";
+    newIngredient.value="";
+    amount.value="";
   }
   
   function submitForm(){
     let name = document.getElementById("meadName").value;
     let abv = document.getElementById("abv").value;
     let honey = document.getElementById("honey").value;
-    let ingredients= document.getElementsByClassName("ingredientSpan");
+    let ingredients= document.getElementsByClassName("ingredientspan");
     let ingredientList = [];
     let started = document.getElementById("started")
     let bottled = document.getElementById("bottled").value;
